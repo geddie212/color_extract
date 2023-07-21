101ca1cb283ab8e59d6be70445dbe1d7cd2781ad
Paul Ged <pauliusgedrimas@gmail.com>
2023-07-21 15:16:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+# A hobby project where the user uploads an image and the web app shows the 10 most used colors in the image
+# Made by Paul Ged
+
+# Import statements
 from flask import Flask, render_template, request
 import os
 from color_calculator import ColorCalculator
 
+# Upload folder
 UPLOAD_FOLDER = 'static/uploads/'
 
+# Flask setup 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
+# Post method which uploads and analyses the photo to extract the 10 most popular colors
 @app.route('/uploader', methods=['GET', 'POST'])
 def upload_img():
     if request.method == 'POST':
@@ -23,11 +29,11 @@ def upload_img():
     return render_template('index.html', palette=palette, file_name=file_name, color_count=color_count,
                            file_size=file_size, file_location=file_location)
 
-
+# Homepage
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
+# Run here
 if __name__ == '__main__':
     app.run(debug=True)
